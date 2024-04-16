fc51f7757fa12d6d0935afd45891df4469b77e62
runner <runner@Mac-1713258835679.local>
2024-04-16 09:14:47 +0000

    Update to scala-deps version to 0.1.4.1

--- scala-deps.rb
+++ scala-deps.rb
@@ -4,13 +4,13 @@
 class ScalaDeps < Formula
   desc "Zat Plugin to fetch the latest stable dependency version for a Scala library"
   homepage "https://github.com/ssanj/scala-deps-zatp"
-  url "https://github.com/ssanj/scala-deps-zatp/archive/refs/tags/v0.6.0.tar.gz"
-  version "0.6.0"
-  sha256 "57f17cb584250e66b8c0f51e870273fdd614260369128563eb38f0c39d70c009"
+  url "https://github.com/ssanj/scala-deps-zatp/archive/refs/tags/v0.1.4.1.tar.gz"
+  version "0.1.4.1"
+  sha256 "ABCDEF123"
   license "BSD-3-Clause"
 
-  depends_on "coursier"
   depends_on "rust" => :build
+  depends_on "coursier"
 
   def install
     system "cargo", "install", *std_cargo_args
@@ -19,6 +19,6 @@ class ScalaDeps < Formula
 
   test do
     output = shell_output("#{bin}/scala-deps -V")
-    assert_equal ["scala-deps 0.6.0\n"], output.lines
+    assert_equal ["scala-deps 0.1.4.1\n"], output.lines
   end
 end
